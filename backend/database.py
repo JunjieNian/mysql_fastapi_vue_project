8902789c8a271cadd6c7a7fbef6408def6fffc99
JunjieNian <nianjerry@126.com>
2026-04-03 09:28:26 +0800

    feat: added urls and modified database.py

--- backend/database.py
+++ backend/database.py
@@ -1,13 +1,23 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.engine import URL
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///sqlite.db"
+SQLALCHEMY_DATABASE_URL = URL.create(
+    "mysql+pymysql",
+    username="root",
+    password="Njj20060901!@#",
+    host="127.0.0.1",
+    port=3306,
+    database="test",
+    query={"charset": "utf8mb4"},
+)
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    echo=True
+    echo=True,
+    pool_pre_ping=True,
 )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
